dad01879d1a588e4e9557b03a712a8e7efd6016d
Carlos Arcos <calinchat@hotmail.com>
2024-07-09 16:47:01 -0600

    Add project files.

---
+++ For.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    public static class For
+    {
+        public static void CiclyFor()
+        {
+            // 1+2+3+4+5+6+7+8+9+10
+            int suma = 0;
+            for(var i = 0; i <= 10; i+=5)
+            {
+                //Quiero la suma de los numero pares de 1 al 20
+                //lo mismo pero de los impares 15 al 30;
+                //La suma de los numero primos!!!! 
+                suma+= i;
+            }
+
+            Console.WriteLine($"La suma es {suma}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using Tutorial;
+
+For.CiclyFor();
+
+
+//int a = 2;
+//var b = 8;
+//bool abierto =  false;
+
+
+//Mayor que >
+//Menor que <
+//Mayor igual >=
+//Meno igual <=
+// Igual que ==
+
+//Operadores Aritmeritocos
+// suma +, resta -, multiplicar *, dividir, % ;
+
+//Console.WriteLine(4 % 3);
+
+//Console.WriteLine("Inserta un numero:");
+//var entrada = Console.ReadLine();
+
+//var edad = int.Parse(entrada);
+
+//if (edad == 0)
+//{
+//    Console.WriteLine($"el numero es 0");
+//}
+//else if (numero > 0)
+//{
+//    Console.WriteLine($"el numero {numero} es positivo");
+//}
+//else
+//{
+//    Console.WriteLine("El numero es Negativo");
+//}
+
+
+//Console.WriteLine("Inserta un numero:");
+//var entrada = Console.ReadLine();
+//var dia = int.Parse(entrada!);
+
+//switch (dia)
+//{
+//    case 1:
+//        Console.WriteLine("Lunes");
+//        break;
+//    case 2:
+//        Console.WriteLine("Martes");
+//        break;
+//    case 3:
+//        Console.WriteLine("Miercoles");
+//        break;
+//    default:
+//        Console.WriteLine("Días no validos");
+//        break;
+
+//}
+
