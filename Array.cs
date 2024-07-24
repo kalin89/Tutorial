d60a2028939743e454f1b61754b4ef9eabc6d7aa
Carlos Arcos <calinchat@hotmail.com>
2024-07-24 12:50:05 -0600

    Arreglos

---
+++ Array.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    public static class ArrayClass
+    {
+        public static void BasicArrayUse()
+        {
+            int[] numeros = new int[2];
+            int[] edades = {23,37,34,12 };
+            //llenar los espacios
+            numeros.Append(0);
+            numeros.Append(4);
+
+            //Length para saber el tamaño del arreglo.
+            Console.WriteLine($"Tamaño: {edades.Length}");
+            int buscar = Array.IndexOf(edades, 12);
+            Console.WriteLine($"La posición de 37 es {buscar}");
+            for (int i = 0; i < edades.Length; i++)
+            {
+                Console.WriteLine($"{i+1} edad {edades[i]}");
+            }
+
+            Console.WriteLine("--------------------------");
+            string[] names = { "Carlos", "Jorge", "Adry", "Citlalli" };
+            //int[] edades = { 23, 37, 34, 12 };
+
+            for(int i= 0 ; i < names.Length; i++)
+            {
+                Console.WriteLine($"La edad de {names[i]} es {edades[i]}");
+            }
+
+            //Ejercicio encuentra el numero mayor
+            int[] numero2 = { -4, -1, -8, -6 };
+
+            int mayor = 0; //Clave esta aquí
+            for(int i= 0 ;i < numero2.Length; i++)
+            {
+                if (numero2[i] > mayor)
+                {
+                    mayor = numero2[i];
+                }
+            }
+
+            Console.WriteLine("El Mayor es {0}", mayor);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,4 +5,5 @@
 using Tutorial;
 
 //Basics.BasicThings();
-For.CiclyFor();
+//For.CiclyFor();
+ArrayClass.BasicArrayUse();
